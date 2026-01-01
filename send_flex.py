6ddda2734600e328e006157fef69fa4827740191
pupipaki <fujifujitatsutatsu@gmail.com>
2026-01-02 08:39:39 +0900

    Rename LINE_TOKEN to CHANNEL_ACCESS_TOKEN

--- send_flex.py
+++ send_flex.py
@@ -2,7 +2,9 @@ import os
 import requests
 import json
 
-LINE_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+# LINE_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+USER_ID = os.getenv("LINE_USER_ID")
 
 def send_flex():
     url = "https://api.line.me/v2/bot/message/push"
